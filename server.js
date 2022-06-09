d04ad1051ff83485c70ca77aca63ed66805e4114
Sarah Malik <sarah.e.mower@gmail.com>
2022-06-09 15:32:52 -0700

    connected database, added post methods

--- server.js
+++ server.js
@@ -1,29 +1,30 @@
 const express = require('express')
-const app = express()
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
+const app = express()
 const connectionString = 'mongodb+srv://semm:ex8R50OjYd65rMgm@cluster0.oxnar.mongodb.net/?retryWrites=true&w=majority'
 
-app.use(bodyParser.urlencoded({extended: true}))
 
 MongoClient.connect(connectionString)
 
 	.then(client => {
 		console.log('Connected to Database')
 		const repertoireDB = client.db('repertoire')
+		app.use(bodyParser.urlencoded({extended: true}))
+		app.use(bodyParser.json())
 
-	app.get('/', (request, response) => {
-		response.sendFile(__dirname + '/index.html')
-	})
-	app.post('/addMusicPiece', (request, response) => {
-		console.log('hello')
-	})
+		app.get('/', (request, response) => {
+			response.sendFile(__dirname + '/index.html')
+		})
+	
+		app.post('/music', (request, response) => {
+  			console.log(request.body)
+		})
 
 
 
-	})
-	.catch(error => console.error(error))
-
+		})
+		.catch(error => console.error(error))
 
 
 
